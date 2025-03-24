97c47bd50c2e258807aa313ef868d66d78ddec5a
kkerra <159480663+kkerra@users.noreply.github.com>
2025-03-24 08:06:31 +0300

    Add files via upload

--- UP11/completed/pr10.sql
+++ UP11/completed/pr10.sql
@@ -1,10 +1,26 @@
+#task1
 CREATE TABLE book_info(
-  `book_info_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) DEFAULT NULL,
-  `surname` varchar(50) DEFAULT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `publication_year` year DEFAULT NULL,
-  `price` decimal(6,2) DEFAULT NULL,
-  `page` smallint DEFAULT NULL,
-  PRIMARY KEY (`my_event_table_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+	book_id INT PRIMARY KEY,
+    title VARCHAR(50),
+    surname VARCHAR(50),
+    `name` VARCHAR(50), 
+    release_year INT, 
+    price DECIMAL(6, 2), 
+    pages_count SMALLINT
+);
+
+#task2
+ALTER TABLE book_info
+MODIFY book_id INT AUTO_INCREMENT;
+
+#task3
+ALTER TABLE book_info
+ADD CONSTRAINT unique_title_author UNIQUE (title, surname, `name`);
+
+#task4
+ALTER TABLE book_info
+ADD arrival_date DATE;
+
+#task5
+ALTER TABLE book_info
+DROP COLUMN pages_count;
